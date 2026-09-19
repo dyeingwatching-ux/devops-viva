b217c4c04a1b3fe9a1ce5623ea3b2822b8c2a27c
Senada wanasinghe <dyeingwatching@gmail.com>
2026-09-19 18:43:25 +0530

    Practice

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+# pyrefly: ignore [missing-import]
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from CSI2113 Practical Assessment!"
+    return "practical 1 test 1"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
