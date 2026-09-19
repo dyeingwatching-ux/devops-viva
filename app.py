864235de8750df86331189873dee5ad8a9946971
Senada wanasinghe <dyeingwatching@gmail.com>
2026-09-19 19:34:18 +0530

    Added the word Hello back to fix the test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "practical 1 test 1"
+    return "Hello! This is a practice run."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
