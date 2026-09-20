97e4f9579a8295d65e681d1e56e655c8edd2d401
Senada wanasinghe <dyeingwatching@gmail.com>
2026-09-20 07:19:36 +0530

    3rd atemot

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello! This is a practice run."
+    return "Hello! This is a practice run 2."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
